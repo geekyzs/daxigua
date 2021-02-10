3a328bb1ec1423eef64f6a6622bd2f67de14fcf2
geekyzs <oncwnuKTgG38U3ss3y1MqZixe_ks@git.weixin.qq.com>
2021-02-11 07:58:27 +0800

    dxg

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -26,10 +26,10 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.9;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
-let fruitSlowDown = false;
+let fruitSlowDown = 5;
 
 // 点击右上方图标更换水果：false 改为 true 即可
 let clickChangeFruit = false;
@@ -38,7 +38,7 @@ let clickChangeFruit = false;
 let adLink = 'https://mp.weixin.qq.com/s/VTSQk3mdVcrQmY0RgP8upA';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜-i日签';
+document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
