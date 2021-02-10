27af15858efb2f9991aaa0a319a24dda8f4d0572
geekyzs <oncwnuKTgG38U3ss3y1MqZixe_ks@git.weixin.qq.com>
2021-02-11 00:19:07 +0800

    v

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,10 +35,10 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = 'https://mp.weixin.qq.com/s/VTSQk3mdVcrQmY0RgP8upA';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成大西瓜-i日签';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
