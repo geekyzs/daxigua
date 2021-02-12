a58bc10d6b789fcba0f1926857630fbea1ceb42a
geekyzs <oncwnuKTgG38U3ss3y1MqZixe_ks@git.weixin.qq.com>
2021-02-13 07:44:20 +0800

    dsg

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,7 +35,7 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://mp.weixin.qq.com/s/VTSQk3mdVcrQmY0RgP8upA';
+let adLink = 'https://mp.weixin.qq.com/s/4vbhzDPEaES_g1niJln0Eg';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
